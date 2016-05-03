202e4903231a4a5f341a0e7dd637700b8b54b113
Stanislav Posonsky <psk@cpositive.ru>
2016-05-03 17:31:05 +0800

    Fonts full circle

--- gulp/fonts.js
+++ gulp/fonts.js
@@ -1,9 +1,12 @@
+/* eslint-env node */
 var gulp = require('gulp'),
   plumber = require('gulp-plumber'),
   notify = require('gulp-notify'),
   del = require('del'),
   flatten = require('gulp-flatten'),
-  chmod = require('gulp-chmod');
+  chmod = require('gulp-chmod'),
+  ttf2woff2 = require('gulp-ttf2woff2'),
+  gulpSequence = require('gulp-sequence');
 
 var config = require('../gulp.json');
 
@@ -13,10 +16,10 @@ var dstFonts = config.theme + config.dsts.fonts,
   });
 
 gulp.task('clean:fonts', function () {
-  return del(dstFonts);
+  return del(dstFonts + '/*');
 });
 
-gulp.task('fonts', ['clean:fonts'], function() {
+gulp.task('fonts:copy', ['clean:fonts'], function() {
   return gulp.src(srcFonts)
     .pipe(plumber({
       errorHandler: notify.onError({
@@ -29,7 +32,26 @@ gulp.task('fonts', ['clean:fonts'], function() {
     .pipe(gulp.dest(dstFonts))
     .pipe(notify({
       title: 'Fonts coped successfully!',
-      message: 'fonts task completed.',
-      onLast: true,
+      message: 'fonts:copy task completed.',
+      onLast: true
     }));
 });
+
+gulp.task('ttf2woff2', function(){
+  gulp.src([dstFonts + '/*.ttf'])
+    .pipe(plumber({
+      errorHandler: notify.onError({
+        title: "Error: Making WOFF2 fonts.",
+        message:"<%= error.message %>"
+      })
+    }))
+    .pipe(ttf2woff2())
+    .pipe(gulp.dest(dstFonts))
+    .pipe(notify({
+      title: 'WOFF2 fonts made successfully!',
+      message: 'ttf2woff2 task completed.',
+      onLast: true
+    }));
+});
+
+gulp.task('fonts', gulpSequence('fonts:copy', 'ttf2woff2'));
